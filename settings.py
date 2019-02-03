f6c508a33ae0ab25f4206ddd616c30d0d2641fec
Tijmen Verhoef <tijmenmenno@gmail.com>
2019-02-03 19:55:13 +0100

    edits

--- settings.py
+++ settings.py
@@ -8,12 +8,6 @@ columns = {
 	"phone": "TEXT"
 }
 
-# Path to your venv activate_this.py
-python_path = "/home/tverhoef/REST-fiesta/APIEnv/bin/activate_this.py"
-
-# Path to your appication
-app_path = "/home/tverhoef/REST-fiesta/app.wsgi"
-
 # Debug options
 host = 'localhost'
 port = 8000
